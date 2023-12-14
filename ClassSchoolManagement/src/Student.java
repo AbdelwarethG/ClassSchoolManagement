4068028ee86fb769ee0593a60ddd091395e05e78
Majdi Ben Youssef <benyoussef.mohamed@esprit.tn>
2023-12-14 23:24:46 +0100

    Majdi Commit

--- ClassSchoolManagement/src/Student.java
+++ ClassSchoolManagement/src/Student.java
@@ -2,6 +2,7 @@ class Student {
     private String studentId;
     private String firstName;
     private String lastName;
+    private float  Moyenne ;
 
     // Constructeur
     public Student(String studentId, String firstName, String lastName, int age) {
@@ -10,7 +11,14 @@ class Student {
         this.lastName = lastName;
     }
 
-    // Getters et Setters
+    public float getMoyenne() {
+        return Moyenne;
+    }
+
+    public void setMoyenne(float moyenne) {
+        Moyenne = moyenne;
+    }
+// Getters et Setters
 
     public String getStudentId() {
         return studentId;
@@ -40,5 +48,19 @@ class Student {
     public void displayStudentInfo() {
         System.out.println("Student ID: " + studentId);
         System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Moyenne "+Moyenne);
+    }
+    public String calculateGrade() {
+        if (Moyenne >= 16.0) {
+            return "A";
+        } else if (Moyenne >= 14.0) {
+            return "B";
+        } else if (Moyenne >= 12.0) {
+            return "C";
+        } else if (Moyenne >= 10.0) {
+            return "D";
+        } else {
+            return "F";
+        }
     }
 }
