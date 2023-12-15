3605f31b9a5a771bb34518b829886a49af708fef
khouloud <khouloudmhamdi7@gmail.com>
2023-12-15 21:43:32 +0100

    Supprimer et modifier

--- ClassSchoolManagement/src/SchoolClass.java
+++ ClassSchoolManagement/src/SchoolClass.java
@@ -26,8 +26,25 @@ public class SchoolClass {
     // MÃ©thode pour ajouter un Ã©tudiant Ã  la classe
     public void ajouterEtudiant(Student student) {
         Students.add(student);
+        System.out.println("--------------Ajout affectué avec succés ----------------");
 
     }
-
+    public void supprimerEtudiant(Student student) {
+        Students.remove(student);
     }
+
+    // Méthode pour modifier le nom de la classe
+    public void modifierNomClasse(String nouveauNom) {
+        this.nom = nouveauNom;
+    }
+
+    // Méthode pour afficher les détails de la classe avec ses étudiants
+    public void afficherDetails() {
+        System.out.println("Classe: " + nom);
+        System.out.println("Étudiants:");
+        for (Student student : Students) {
+            student.displayStudentInfo();
+            System.out.println("--------------");
+        }
+    }}
 
