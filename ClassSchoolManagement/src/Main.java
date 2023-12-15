88df050aaa4865e8a49b8f81e8d9062ced7655d4
Wareth Ghribi <ghribi.abdelwareth@esprit.tn>
2023-12-15 21:34:18 +0100

    Main code

--- ClassSchoolManagement/src/Main.java
+++ ClassSchoolManagement/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Premier Test");
+
+        SchoolClass classe1 = new SchoolClass("Classe A");
+
+
+        Student etudiant1 = new Student("S001", "John", "Doe", 20);
+        Student etudiant2 = new Student("S002", "Alice", "Smith", 22);
+
+
+        classe1.ajouterEtudiant(etudiant1);
+        classe1.ajouterEtudiant(etudiant2);
+
+
+        System.out.println("Détails initiaux de la classe:");
+        classe1.afficherDetails();
+
+
+        classe1.modifierNomClasse("Classe B");
+
+
+        Student etudiant3 = new Student("S003", "Bob", "Johnson", 21);
+
+
+        classe1.ajouterEtudiant(etudiant3);
+
+
+        classe1.supprimerEtudiant(etudiant1);
+
+
+        System.out.println("\nDétails mis à jour de la classe:");
+        classe1.afficherDetails();
     }
 }
